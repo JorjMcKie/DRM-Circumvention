ce4fe997888bffde568dd8f6c0ecb311c73fac71
JorjMcKie <jorj.x.mckie@outlook.de>
2017-06-07 16:15:56 -0400

    Add script based on PyAutoGUI

--- README.md
+++ README.md
@@ -8,12 +8,12 @@ It works as follows:
 
 1. Open your e-book application with the book you want to backup
 2. Start one of the scripts provided here
-3. Choose whether you want to actually start the backup process, or just display an example page image
-4. Use the test page display to adjust the screen region such that the displayed book page is properl covered.
-5. Once the backup process is started, it will automatically flip through the book and make an image of every page. Each image will become a new PDF page.
-6. If end of book is encountered (recognized as two pages in a row which are equal down to the pixel level), a new PDF ``ebook.pdf`` will be saved.
+3. Choose whether you want to actually start the backup process, or just display an example page.
+4. Use the test page display to adjust the screen region such that the displayed book page is properly covered.
+5. Once the backup process is started, it will automatically flip through the book, make an image of every page, and store it as a new PDF page.
+6. If end of book is encountered (recognized as two pages in a row which are equal down to the pixel level), a new PDF ``ebook.pdf`` will be saved in the script directory.
 
-The new PDF will be compressed, but expect something like 10 MB per each 100 pages. Just as if you would create a PDF by scanning material.
+The new PDF will be compressed, but expect something like 10 MB per each 100 pages. Just as if you would create a PDF by scanning some material.
 
 After you are done, you may consider using an OCR tool to further process the new PDF content ...
 
@@ -21,8 +21,8 @@ After you are done, you may consider using an OCR tool to further process the ne
 Apart from PyMuPDF (which does all the PDF work), you need components to
 
 1. take an image of the selected screen area
-2. send page forward your e-book reader
+2. send page forward to your e-book reader
 
 For (1.) you can e.g. use ``Pillow.ImageGrab`` (available on MacOS and Windows). For (2.) you can use `SendKeys` (available on Windows Python 2 only).
 
-`PyAutoGUI` (https://pypi.python.org/pypi/PyAutoGUI/0.9.36) claims to cover both of the above (not verified).
+I highly recommend `PyAutoGUI` (https://pypi.python.org/pypi/PyAutoGUI/0.9.36) which covers both of the above in the most elegant way. It works on any bitness of Windows, Mac and Linux, Python 2 and Python 3. I have tested it on Windows 32bit and 64bit for Python 2 and 3.
---
+++ scan_autogui.py
@@ -0,0 +1,77 @@
+from __future__ import print_function
+import pyautogui
+import fitz
+import time, sys
+"""
+@created: 2017-06-05 18:00:00
+
+@author: (c) 2017 Jorj X. McKie
+
+Create a readable PDF copy of any (potentially DRM protected) pageable content
+-------------------------------------------------------------------------------
+
+Dependencies:
+-------------
+PyMuPDF v1.11.0+, pyautogui
+
+
+License:
+--------
+GNU GPL V3+
+
+Description
+------------
+Page through the display of an e-book and store each page as a full page image
+in a new PDF.
+
+The page image grabbing and paging through the book is done with pyautoit.
+"""
+if sys.version_info[0] < 3:
+    getResponse = raw_input
+else:
+    getResponse = input
+
+
+# This is the rectangle copied. To be adjusted until it fits ...!
+# PyAutoGUI region parameter expects (left, top, width, height)
+bbox = (70, 100, 850-70, 1008-100)     # the screen area to grab
+width = bbox[2] / 2.         # determines PDF page width
+height = bbox[3] / 2.        # ... and page height
+
+# If you need to test your bbox image, answer "y"
+answer = getResponse("need test picture? y/n >")
+if answer == "y":
+    print("you have 5 seconds to activate the e-book window ...")
+    time.sleep(5)
+    print("grabbing picture now")
+    img = pyautogui.screenshot(region = bbox)
+    img.show()
+    raise SystemExit
+
+print("you have 5 seconds to activate the e-book window ...")
+time.sleep(5)    
+print("starting process now")
+print("Don't touch! The e-book window needs focus all the time!")
+
+doc = fitz.open()                      # new empty PDF
+old_samples = None                     # used to check end of e-book
+i = 0
+while 1:
+    img = pyautogui.screenshot(region = bbox)  # get the displayed e-book page
+    samples = img.tobytes()            # copy for PyMuPDF
+    if samples == old_samples:         # if no change: must be end of book
+        print("end of book encountered ... finishing")
+        break
+    old_samples = samples              # store this page image
+    pix = fitz.Pixmap(fitz.csRGB, img.size[0], img.size[1], samples, 0)
+    doc.insertPage(-1, width = width, height = height)     # new PDF page
+    page = doc[-1]                     # load it as a page object
+    page.insertImage(page.rect, pixmap = pix)    # insert screen print as image
+    pyautogui.hotkey("pagedown")       # next page in e-book reader
+    time.sleep(2)                      # allow for slow response time
+    i += 1
+
+# new PDF now contains page images as if scanned-in.
+doc.save("ebook.pdf", deflate=True)     # save our PDF copy with compression
+print(i, "pages saved.")
+    
