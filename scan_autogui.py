62eb90947cf8cb791fc3a9351066b1932f70cf4c
Jorj X. McKie <jorj.x.mckie@outlook.de>
2018-08-04 04:28:12 -0400

    Apply desktop changes

--- scan_autogui.py
+++ scan_autogui.py
@@ -32,26 +32,28 @@ else:
     getResponse = input
 
 
-# This is the rectangle copied. To be adjusted until it fits ...!
-# PyAutoGUI region parameter expects (left, top, width, height)
+# This is the rectangle copied. You should adjust it until it fits!
+# PyAutoGUI region parameter expects
+#      (left, top, width, height)
 bbox = (70, 100, 850-70, 1008-100)     # the screen area to grab
+
 width = bbox[2] / 2.         # determines PDF page width
 height = bbox[3] / 2.        # ... and page height
 
 # If you need to test your bbox image, answer "y"
-answer = getResponse("need test picture? y/n >")
+answer = getResponse("need a test picture? y/n >")
 if answer == "y":
-    print("you have 5 seconds to activate the e-book window ...")
+    print("activate the e-book window NOW (5 sec) ...")
     time.sleep(5)
     print("grabbing picture now")
     img = pyautogui.screenshot(region = bbox)
     img.show()
     raise SystemExit
 
-print("you have 5 seconds to activate the e-book window ...")
+print("activate / click the e-book window NOW (5 sec) ...")
 time.sleep(5)    
 print("starting process now")
-print("Don't touch! The e-book window needs focus all the time!")
+print("Hands off! The e-book window needs focus all the time!")
 
 doc = fitz.open()                      # new empty PDF
 old_samples = None                     # used to check end of e-book
@@ -59,13 +61,12 @@ i = 0
 while 1:
     img = pyautogui.screenshot(region = bbox)  # get the displayed e-book page
     samples = img.tobytes()            # copy for PyMuPDF
-    if samples == old_samples:         # if no change: must be end of book
+    if samples == old_samples:         # if no change: assume end of book
         print("end of book encountered ... finishing")
         break
     old_samples = samples              # store this page image
-    pix = fitz.Pixmap(fitz.csRGB, img.size[0], img.size[1], samples, 0)
-    doc.insertPage(-1, width = width, height = height)     # new PDF page
-    page = doc[-1]                     # load it as a page object
+    pix = fitz.Pixmap(fitz.csGRAY, img.size[0], img.size[1], samples, 0)
+    page = doc.newPage(-1, width = width, height = height)
     page.insertImage(page.rect, pixmap = pix)    # insert screen print as image
     pyautogui.hotkey("pagedown")       # next page in e-book reader
     time.sleep(2)                      # allow for slow response time
